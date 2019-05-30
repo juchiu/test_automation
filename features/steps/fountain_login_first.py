45416f3f145e6c979cdcd92221943d4c30e5c6ca
ritata12 <46690528+ritata12@users.noreply.github.com>
2019-05-30 10:18:20 -0700

    Update fountain_login_first.py

--- features/steps/fountain_login_first.py
+++ features/steps/fountain_login_first.py
@@ -6,16 +6,11 @@ from time import sleep
 USER_EMAIL = (By.ID, 'user_email')
 PASSWORD = (By.ID, 'user_password')
 LOGIN_BTN = (By.CSS_SELECTOR, 'input.btn.btn-submit')
-TROUBLE_SIGN_IN_LINK = (By.XPATH, "//a[contains(@href,'trouble_sign_in')]")
 
-# def create_emails_list(email) -> list:
-#     print(email)
-#     return [usernameStr[0], usernameStr[1], usernameStr[2].format.(email)]
-#
+
+
 usernameStr = ['engineering@onboardiq.dev']
 passwordStr = ['martiansworkingiq']
-# for x in range(4):
-# # driver.find_element(*USER_EMAIL).send_keys(usernameStr.format(x))
 
 @Given('Open login page')
 def open_page(context):
@@ -40,8 +35,3 @@ def click_login(context):
     context.driver.find_element(*LOGIN_BTN).click()
     sleep(1)
 
-#
-# @then('fountain.com signin page opens')
-# def verify_signin_opens(context):
-#     context.driver.find_element(*LOGIN_BTN)
-#     context.driver.find_element(*TROUBLE_SIGN_IN_LINK)
