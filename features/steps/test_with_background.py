a021911b1d1b0e292c24e1d5383cbe43b8f8d32e
ritata12 <46690528+ritata12@users.noreply.github.com>
2019-05-30 10:12:47 -0700

    Delete test_with_background.py

--- features/steps/test_with_background.py
+++
@@ -1,8 +0,0 @@
-# from behave import given, when
-# from selenium.webdriver.common.by import By
-#
-# DROPDOWN = (By.CSS_SELECTOR, "button.obiq-dropdown__toggle")
-#
-# @when('Click jobs dropdown')
-# def click_jobs(context):
-#     context.driver.find_element(*DROPDOWN)
